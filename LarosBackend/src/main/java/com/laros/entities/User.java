5fe2bf8220ffa350fb609f2eac8bf140032564f1
Luis MX <ing.arellano@gmail.com>
2026-03-03 08:49:39 -0300

    develop: changed configuration for postgres db.

--- LarosBackend/src/main/java/com/laros/entities/User.java
+++ LarosBackend/src/main/java/com/laros/entities/User.java
@@ -1,12 +1,9 @@
 package com.laros.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id    @GeneratedValue(strategy = GenerationType.IDENTITY)
